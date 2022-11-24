37db9f523f3759594ea511cda1cf5dc85ed7d7e2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 00:05:12 +0000

    Adding tarfile member sanitization to extractall()

--- storage_service/common/management/commands/import_aip.py
+++ storage_service/common/management/commands/import_aip.py
@@ -214,7 +214,26 @@ def _decompress(aip_path, temp_dir):
 def _decompress_tar_gz(aip_path, temp_dir):
     with tarfile.open(aip_path) as tar:
         aip_root_dir = os.path.commonprefix(tar.getnames())
-        tar.extractall(path=temp_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=temp_dir)
     return os.path.join(temp_dir, aip_root_dir)
 
 
